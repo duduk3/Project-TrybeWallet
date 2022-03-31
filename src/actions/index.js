63562bdeabd8a096e8d81a8f82e565826d15a8df
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-03-31 15:44:41 -0300

    configuraçaoinicial da store, reducers e actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,6 @@
 // Coloque aqui suas actions
+export const ADD_USER = 'ADD_USER';
+export const ADD_WALLET = 'ADD_WALLET';
+
+export const actionAddUser = (data) => ({ type: ADD_USER, data });
+export const actionAddWallet = (data) => ({ type: ADD_WALLET, data });
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,22 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { ADD_WALLET } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case ADD_WALLET:
+    return {
+      ...state,
+      wallet: action.wallet.state,
+    };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
